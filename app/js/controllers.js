294cd01db70e04c9ecfb219e0e0825aab828ab8d
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-08-04 22:14:31 -0400

    step 5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,3 +17,13 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
 
   $scope.orderProp = 'age';
 });
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+});
